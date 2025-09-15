f8f8ffa4d8090cf54037775e2ce65f9fc4c5979a
talibDennis <joseph.dennis@triosstudent.com>
2025-09-14 23:29:43 -0400

    Updated Store.java

---
+++ Store.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Store {
+  private ContentServer server;
+
+  public Store(ContentServer server) {
+    this.server = server;
+  }
+
+  public List<Content> listAvailableMovies() {
+    return server.fetchavAilableContent();
+  }
+
+  public double calculateTotalPrice(List<Content> selectedMovies) {
+    return selectedMovies.stream().mapToDouble(Content::getPrice).sum();
+  }
+
+  public double estimateDownloadTime(List<Content> selectedMovies, double speedMbps) {
+    double totalSizeMB = selectedMovies.stream().mapToDouble(Content::getSizeInMB).sum();
+    double speedMBps = speedMbps / 8.0;
+    return totalSizeMB / speedMBps;
+  }
+}
